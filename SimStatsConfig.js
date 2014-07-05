5ddcc9ae7af413c160828dd69aaf802c3693bec0
Robert Adams <misterblue@misterblue.com>
2014-07-05 07:50:58 -0700

    Initial SimStats version

---
+++ SimStatsConfig.js
@@ -0,0 +1,10 @@
+// Configuration file for the grid that Stats is displaying.
+var gGridName = "Roberts Grid";
+
+var gServers = {};
+gServers["BulletSim"]       = "http://blueland.virtualportland.org:14499";
+
+// Seconds between data fetches.
+var gConnPullInterval = 10;     // status poll time in seconds
+var gScenePullInterval = 10;     // status poll time in seconds
+var gServerPullInterval = 30;     // status poll time in seconds
