1c0817df3e6a781545c66f7921981cf8724dbcbf
Robert Adams <misterblue@misterblue.com>
2014-07-05 13:03:29 -0700

    Add dataTables as a local library. Clean up DoPush syntax

--- DoPush.sh
+++ DoPush.sh
@@ -7,7 +7,13 @@ DIR="SimStats"
 
 DST="${ACCT}@${SITE}:${DIR}/"
 
-FILES="index.html jquery.sparkline.min.js jquery-2.1.1.min.js SimStats.css SimStatsConfig.js WhiteTexture.png"
+FILES="index.html"
+FILES="$FILES jquery-2.1.1.min.js"
+FILES="$FILES jquery.sparkline.min.js"
+FILES="$FILES jquery.dataTables.1.10.0.js jquery.dataTables.1.10.0.css"
+FILES="$FILES SimStats.css SimStatsConfig.js"
+FILES="$FILES WhiteTexture.png"
 
 echo "rsync'ing files to $DST"
 rsync $FILES "$DST"
+
